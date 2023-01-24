c33ad936e1a3140ee11a618fff3e295a46f65fda
Calpurniax <sofia.gracia@gmail.com>
2023-01-24 20:29:23 +0100

    create header component add css variables and reset

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,9 +1,23 @@
 import '../styles/App.scss';
+import Header from './Header';
 
 function App() {
   return (
     <div className='App'>
-      <h1>madre mía</h1>
+      <Header />
+      <main>
+        <section class="calendar">
+          <h2 class="calendar__title">aquí el calendario</h2>
+
+          <div class="calendar_move">
+
+          </div>
+          <div class="calendar_options">Aquí irían los demás meses, sólo cambiaría de la página, el mes</div>
+          <div class="calendar_leyend">aqui como se lee el calendario</div>
+        </section>
+        <aside>lista de plantitas</aside>
+
+      </main>
     </div>
   );
 }
